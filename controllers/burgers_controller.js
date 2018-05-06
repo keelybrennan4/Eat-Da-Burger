3f1f268348dfde7b21830ba5487918418f062ceb
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-05-06 17:57:36 -0500

    router put and post routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,21 +7,44 @@ var burger = require("../models/burger.js");
 //// Create all routes and set up logic within those routes
 
 //render index within views folder 
-router.get('/', function(req, res){
-    burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render('index');
-    })
-})
+router.get("/", function(req, res){
+    burger.all(function(data){
+        //console.log(data);
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-//router.post()
-//router.put()
+router.post("/api/burgers", function (req, res){
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId});
+    });
+});
+
+router.put("api/burgers/:id", function (req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+    });
+});
+
+
 //router.delete()
-
-
-
-
-
 
 // export routes for server.js to use.
 module.exports = router;
