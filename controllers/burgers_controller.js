2a7f0d7a2dbddc0845e3aca83b6f9f5637b543d7
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-05-07 00:24:39 -0500

    updated hbs index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,23 +7,25 @@ var burger = require("../models/burger.js");
 //render index within views folder 
 router.get("/", function(req, res){
     burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render("index", {burger_data});
+        //console.log(burger_data);
+        var hbsObj = {
+            burgers: data
+        };
+        for (i=0; i<hbsObj.burgers.length; i++){
+            hbsObj.burgers[i].devoured = parseInt(hbsObj.burgers[i].devoured);
+        }
+        res.render("index", hbsObj);
     });
 });
 
-router.post("/burgers/create", function (req, res){
-    burger.create(req.body.burger_name, function(result){
-        res.redirect("/");
-    })
+router.post("/", function (req, res){
+    burger.create(req.body.newBurger);
+    res.redirect("/");
 });
 
-// Update the status to devoured
-router.put("/burgers/update", function(req, res) {
-    burger.update(req.body.id, function(result){
-        console.log(result);
-        res.redirect("/");
-    });
+router.put("/:id", function(req, res) {
+    burger.update(req.params.id);
+    res.redirect("/");
 });
 
 // export routes for server.js to use.
